1472990e2a7e831b2f1f8c6ee76d47db2b147335
ishugaepov <ilnur.shug@gmail.com>
2021-01-26 12:36:42 +0300

    spark notebooks

---
+++ lectures/spark/README.md
@@ -0,0 +1,5 @@
+# Задание
+## Знакомство со Spark и HDFS
+
+Выполните упражнения из файла [spark_examples.ipynb](/lectures/spark/notebooks)
+
---
+++ lectures/spark/notebooks/utils.py
@@ -0,0 +1,6 @@
+class Rating:
+    def __init__(self, user_id, movie_id, rating, timestamp):
+        self.user_id = user_id
+        self.movie_id = movie_id
+        self.rating = rating
+        self.timestamp = timestamp
